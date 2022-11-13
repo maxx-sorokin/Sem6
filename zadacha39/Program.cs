cd5a0b2917ac640d26eae88c0ee691f40fee2896
Maxim Sorokin <maximka773@mail.ru>
2022-11-13 10:41:32 +0300

    zadacha39

--- zadacha39/Program.cs
+++ zadacha39/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] array = new string[3] { "Vitalya", "Vasya", "Ivan" };
+
+string[] reversedArray = ReverseArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(reversedArray);
+
+string[] ReverseArray(string[] array)
+{
+    string[] reversed = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        reversed[i] = array[array.Length - 1 - i];
+    }
+    return reversed;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
